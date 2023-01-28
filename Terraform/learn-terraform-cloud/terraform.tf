60a4fdb22311c4b0fbbc8dd69fae034e7e993d62
MrMejia24 <felix.mejia24@gmail.com>
2023-01-27 19:22:52 -0500

    Added Terraform folder

---
+++ Terraform/learn-terraform-cloud/terraform.tf
@@ -0,0 +1,20 @@
+/*
+terraform {
+
+  cloud {
+    organization = "mrmejia24-cloud"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+*/
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
